4279a599d17c8de883b1654d6bcb40c3980976ba
Carlos01333R <carlosrodriguezanavila0@gmail.com>
2024-09-04 16:51:06 -0500

    add to page response

--- src/page/response.jsx
+++ src/page/response.jsx
@@ -23,7 +23,13 @@ const PaymentDetails = () => {
             throw new Error("Network response was not ok");
           }
           const data = await response.json();
-          setPaymentDetails(data);
+
+          // Verificar si la respuesta contiene los datos esperados
+          if (data.success && data.data) {
+            setPaymentDetails(data.data);
+          } else {
+            setError("No se encontraron detalles para esta referencia.");
+          }
         } catch (error) {
           setError(error.message);
         } finally {
@@ -43,24 +49,45 @@ const PaymentDetails = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <a href="http://localhost:5173/">Home</a>
+      <a href="https://sorteopy.vercel.app/">Home</a>
       <h1 className="text-2xl font-bold">Detalles del Pago {refPayco}</h1>
       {paymentDetails ? (
         <div>
           <p>
-            <strong>ID de Transacción:</strong> {paymentDetails.Referencia}
+            <strong>ID de Transacción:</strong>{" "}
+            {paymentDetails.x_transaction_id}
           </p>
           <p>
-            <strong>Fecha:</strong> {paymentDetails.Fecha}
+            <strong>Fecha:</strong> {paymentDetails.x_fecha_transaccion}
           </p>
           <p>
-            <strong>Estado:</strong> {paymentDetails.Respuesta}
+            <strong>Estado:</strong> {paymentDetails.x_respuesta}
           </p>
           <p>
-            <strong>Monto:</strong> {paymentDetails.Total}
+            <strong>Monto:</strong> {paymentDetails.x_amount}
           </p>
-
-          {/* Muestra otros detalles necesarios aquí */}
+          <p>
+            <strong>Descripción:</strong> {paymentDetails.x_description}
+          </p>
+          <p>
+            <strong>Nombre del Banco:</strong> {paymentDetails.x_bank_name}
+          </p>
+          <p>
+            <strong>Código de Aprobación:</strong>{" "}
+            {paymentDetails.x_approval_code}
+          </p>
+          <p>
+            <strong>Tarjeta:</strong> {paymentDetails.x_cardnumber}
+          </p>
+          <p>
+            <strong>Cliente:</strong> {paymentDetails.x_customer_name}{" "}
+            {paymentDetails.x_customer_lastname}
+          </p>
+          <p>
+            <strong>Email del Cliente:</strong>{" "}
+            {paymentDetails.x_customer_email}
+          </p>
+          {/* Agrega otros detalles relevantes según tus necesidades */}
         </div>
       ) : (
         <p>No se encontraron detalles para esta referencia.</p>
